5b0306240f50683eef7fba86374a438cb978db47
Alberto Velaz <albertovelaz.dev@gmail.com>
2018-03-18 22:21:41 +0100

    Add DeckItem component

---
+++ components/DeckItem.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class DeckItem extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Desk Item
+        </Text>
+      </View>
+    )
+  }
+}
+
+export default DeckItem
