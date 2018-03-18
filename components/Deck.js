535621e344943d1aef93f2ded40b900972fbd530
Alberto Velaz <albertovelaz.dev@gmail.com>
2018-03-18 22:21:30 +0100

    Add Deck component

---
+++ components/Deck.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class Deck extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Desk View
+        </Text>
+      </View>
+    )
+  }
+}
+
+export default Deck
