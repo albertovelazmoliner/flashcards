0cb4073751c2c1d53fb21fc746d62a428b1e82bc
Alberto Velaz <albertovelaz.dev@gmail.com>
2018-03-18 22:18:39 +0100

    Add DecksList component

---
+++ components/DecksList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class DecksList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          No desks yets
+        </Text>
+      </View>
+    )
+  }
+}
+
+export default DecksList
