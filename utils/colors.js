646926e529c8dc0e6edfa2cd83eb80ac9dc70a09
Alberto Velaz <albertovelaz.dev@gmail.com>
2018-03-18 22:18:12 +0100

    Add colors

---
+++ utils/colors.js
@@ -0,0 +1,6 @@
+export const white = '#ffffff'
+export const black = '#000000'
+export const red = '#b71845'
+export const orange = '#f26f28'
+export const blue = '#4e4cb8'
+export const gray = '#757575'
