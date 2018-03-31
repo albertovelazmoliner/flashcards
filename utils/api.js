935d43ee830878ff40dbcbbd8a2725b92d6a32e5
Alberto Velaz <albertovelaz.dev@gmail.com>
2018-04-01 00:38:40 +0200

    Fix addCardToDeck function in api

--- utils/api.js
+++ utils/api.js
@@ -9,6 +9,7 @@ export const getDecks = () =>  {
 export const getDeck = id => {
   return AsyncStorage.getItem(DECK_STORAGE_KEY)
   .then(results => {
+    console.log(results)
     return results[id]
   })
 }
@@ -24,15 +25,10 @@ export const saveDeck = deckTitle => {
 }
 
 export const addCardToDeck = (title, card) => {
-  return AsyncStorage.mergeItem(DECK_STORAGE_KEY, JSON.stringify({
-    [title]: {
-      questions: [
-        ...questions,
-        {
-          question: card.question,
-          answer: card.answer
-        }
-      ]
-    }
-  }))
+  return AsyncStorage.getItem(DECK_STORAGE_KEY)
+  .then(results => {
+    const data = JSON.parse(results)
+    data[title].questions.push(card)
+    return AsyncStorage.setItem(DECK_STORAGE_KEY, JSON.stringify(data))
+  })
 }
