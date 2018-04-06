da557d9b2abd213ce3402dedb470fea0752dc763
Alberto Velaz <albertovelaz.dev@gmail.com>
2018-04-06 15:31:10 +0200

    Add Android Ripple to DeckItem component

--- components/DeckItem.js
+++ components/DeckItem.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, TouchableNativeFeedback, Platform } from 'react-native'
 import PropTypes from 'prop-types'
 import { white, blue } from '../utils/colors'
 
@@ -15,14 +15,28 @@ class DeckItem extends Component {
     const {title, questions} = deck
 
     return (
-      <TouchableOpacity onPress={() => this.props.onPress(deck)}>
-        <View style={[styles.card, {backgroundColor: white}]}>
-          <Text style={{color: blue}}>{title}</Text>
-          <Text style={{color: blue}}>
-            {questions ? `${questions.length} questions` : "No questions" }
-          </Text>
-        </View>
-      </TouchableOpacity>
+      <View>
+        {Platform.OS === 'ios' && 
+          <TouchableOpacity onPress={() => this.props.onPress(deck)}>
+          <View style={[styles.card, {backgroundColor: white}]}>
+            <Text style={{color: blue}}>{title}</Text>
+            <Text style={{color: blue}}>
+              {questions ? `${questions.length} questions` : "No questions" }
+            </Text>
+          </View>
+        </TouchableOpacity>}
+        {Platform.OS != 'ios' && 
+          <TouchableNativeFeedback 
+            onPress={() => this.props.onPress(deck)}
+            background={TouchableNativeFeedback.Ripple(blue)}>
+          <View style={[styles.card, {backgroundColor: white}]}>
+            <Text style={{color: blue}}>{title}</Text>
+            <Text style={{color: blue}}>
+              {questions ? `${questions.length} questions` : "No questions" }
+            </Text>
+          </View>
+        </TouchableNativeFeedback>}
+      </View>
     )
   }
 }
